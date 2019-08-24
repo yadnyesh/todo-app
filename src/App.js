cc32e4b19495422835437eeb4286229057fea60f
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-08-24 23:37:08 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       My Hello World!
-//     </div>
-//   );
-// }
-
 class App extends Component {
   render() {
     return (
