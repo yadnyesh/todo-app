63c1aad52edee0aca05919372ea638fc4bba1221
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-08-25 00:05:39 +0530

    14 - Create Counter component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FirstComponent from './components/learning-examples/FirstComponent'
+import Counter from './components/counter/Counter'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        My Hello World!
-        <FirstComponent />
-        <SecondComponent />
+        <Counter />
       </div>
     );
   }
@@ -22,5 +21,18 @@ function SecondComponent() {
     </div>
   );
 }
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="learningComponents">
+        My Hello World!
+        <FirstComponent />
+        <SecondComponent />
+      </div>
+    );
+  }
+}
+
 
 export default App;
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+function Counter() {
+    return (
+      <div className="counters">
+        Counter
+      </div>
+    );
+  }
+
+export default Counter  
