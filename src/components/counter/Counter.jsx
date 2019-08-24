19aff61a84b74d28184abfdeda084e1ec0925218
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-08-25 00:34:00 +0530

    19 - Counter App

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -7,7 +7,7 @@ class Counter extends Component {
     constructor() {
         super();
         this.state = {
-            counter: 0
+            counter: 0        
         }
         this.increment = this.increment.bind(this)
     }
