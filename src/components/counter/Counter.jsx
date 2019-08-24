ecd05a560602b78f79336982dd1a1ad32f79f4bd
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-08-25 00:22:43 +0530

    Implemented Counter

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -2,13 +2,33 @@ import React, {Component} from 'react'
 import './Counter.css'
 
 
-function Counter() {
-    return (
-      <div className="counters">
-        <button>+1</button><br></br>
-        <span className="count">0</span>
-      </div>
-    );
+class Counter extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            counter: 0
+        }
+        this.increment = this.increment.bind(this)
+    }
+    
+    render() {
+        return (
+        <div className="counters">
+            <button onClick={this.increment}>+1</button><br></br>
+            <span className="count">{this.state.counter}</span>
+        </div>
+        );
+    }
+
+    increment() {
+        console.log('Increment by 1');
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    }  
+    
   }
+
 
 export default Counter  
