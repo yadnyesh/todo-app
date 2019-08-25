0d0c5b7e891ca1d6053b2b0dbaa7873c07018dc6
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-08-25 08:28:50 +0530

    Make Change event common component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -18,29 +18,37 @@ class LoginComponent extends Component {
             username: "in28minutes",
             password: ""
         }
-        this.handleUsernameChange = this.handleUsernameChange.bind(this)
-        this.handlePasswordChange = this.handlePasswordChange.bind(this)    
+        // this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        // this.handlePasswordChange = this.handlePasswordChange.bind(this)  
+        this.handleChange = this.handleChange.bind(this)  
     }
 
-    handleUsernameChange(event) {
+    handleChange(event) {
         console.log(event.target.value);
         this.setState({
             [event.target.name]:event.target.value
         })
     }
+
+    // handleUsernameChange(event) {
+    //     console.log(event.target.value);
+    //     this.setState({
+    //         [event.target.name]:event.target.value
+    //     })
+    // }
  
-    handlePasswordChange(event) {
-        console.log(event.target.value);
-        this.setState({
-            [event.target.name]:event.target.value
-        })
-    }
+    // handlePasswordChange(event) {
+    //     console.log(event.target.value);
+    //     this.setState({
+    //         [event.target.name]:event.target.value
+    //     })
+    // }
 
     render() {
         return (
             <div>
-                User Name: <input type = "text" name="username" value={this.state.username} onChange={this.handleUsernameChange}/>
-                Password: <input type = "password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
+                User Name: <input type = "text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                Password: <input type = "password" name="password" value={this.state.password} onChange={this.handleChange}/>
                 <button>Login</button>
             </div>
         );
