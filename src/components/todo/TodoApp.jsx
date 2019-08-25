fcceacd6b1a27ec70ebda0b72e7fc1b9a96f5ca0
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-08-25 08:46:18 +0530

    HardCoded Authentication

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -16,9 +16,12 @@ class LoginComponent extends Component {
         super(props)
         this.state = {
             username: "in28minutes",
-            password: ""
+            password: "dummy",
+            hasLoginfailed: false,
+            showSuccessMessage: false
         }
-        this.handleChange = this.handleChange.bind(this)  
+        this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
     }
 
     handleChange(event) {
@@ -28,15 +31,43 @@ class LoginComponent extends Component {
         })
     }
 
+    loginClicked() {
+        if(this.state.username==="in28minutes" && this.state.password==="dummy"){
+            this.setState({showSuccessMessage:true})
+            this.setState({hasLoginfailed:false})
+        }
+        else {
+            this.setState({showSuccessMessage:false})
+            this.setState({hasLoginfailed:true}) 
+        }    
+    }
+
     render() {
         return (
             <div>
+                <ShowInvalidCredentials hasLoginfailed={this.state.hasLoginfailed}/>
+                <DisplaySuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>
                 User Name: <input type = "text" name="username" value={this.state.username} onChange={this.handleChange}/>
                 Password: <input type = "password" name="password" value={this.state.password} onChange={this.handleChange}/>
-                <button>Login</button>
+                <button onClick={this.loginClicked}>Login</button>
             </div>
         );
     }
 }
+
+function ShowInvalidCredentials(props) {
+    if(props.hasLoginfailed) {
+        return <div>Invalid Credentials</div>
+    }
+    return null
+}
+
+function DisplaySuccessMessage(props) {
+    if(props.showSuccessMessage){
+        return <div>Login Successful</div>
+    }
+    return null
+}
+
 
 export default TodoApp
