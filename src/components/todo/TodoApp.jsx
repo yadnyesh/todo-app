3f1b59e0e1ea4b0f14e72d59cd8814a90a4345c2
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-08-25 11:29:23 +0530

    Implement Routing

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,15 +1,31 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class TodoApp extends Component {
     render() {
         return (
             <div className="secondComponent">
-              <LoginComponent />
+            <Router>
+                <>
+                    <Route path="/" exact component={LoginComponent}/>
+                    <Route path="/login" component={LoginComponent}/>
+                    <Route path="/welcome" component={WelcomeComponent}/>
+                </>
+            </Router>
             </div>
           );
     }
 }
 
+class WelcomeComponent extends Component {
+    render () {
+        return(
+            <div>
+                Welcome in the Welcome Component
+            </div>
+        );
+    }
+}
 
 class LoginComponent extends Component {
     constructor(props) {
