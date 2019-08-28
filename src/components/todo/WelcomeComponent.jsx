5418d528048618c28a1834d977dd87ec09f64ee9
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-08-28 06:02:54 +0530

    60 - Add button

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -7,11 +7,20 @@ class WelcomeComponent extends Component {
             <>
                 <h1>Welcome! {this.props.match.params.name}</h1>
                 <div className="container">
-                You can manage your todos <Link to="/todos">here</Link>.
+                    You can manage your todos <Link to="/todos">here</Link>.
+                </div>
+                <div className="container">
+                    Click here to get customized welcome messsage.
+                    <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
                 </div>
             </>
         );
     }
+
+    retrieveWelcomeMessage() {
+        console.log('Retrieve Welcome message')
+    }
 }
+
 
 export default WelcomeComponent
