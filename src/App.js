de2b8bb6337c327d391a5305d099c147b93005ff
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-08-28 06:11:53 +0530

    Refactor and Use Port 4200

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './bootstrap.css';
-
-import FirstComponent from './components/learning-examples/FirstComponent'
-import Counter from './components/counter/Counter'
 import TodoApp from './components/todo/TodoApp'
 
 class App extends Component {
@@ -18,25 +14,25 @@ class App extends Component {
   }
 }
 
-function SecondComponent() {
-  return (
-    <div className="secondComponent">
-      My Hello World from THIRD Component!
-    </div>
-  );
-}
+// function SecondComponent() {
+//   return (
+//     <div className="secondComponent">
+//       My Hello World from THIRD Component!
+//     </div>
+//   );
+// }
 
-class LearningComponents extends Component {
-  render() {
-    return (
-      <div className="learningComponents">
-        My Hello World!
-        <FirstComponent />
-        <SecondComponent />
-      </div>
-    );
-  }
-}
+// class LearningComponents extends Component {
+//   render() {
+//     return (
+//       <div className="learningComponents">
+//         My Hello World!
+//         <FirstComponent />
+//         <SecondComponent />
+//       </div>
+//     );
+//   }
+// }
 
 
 export default App;
--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,7 +1,12 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-
 class WelcomeComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this);
+    }
+
     render () {
         return(
             <>
